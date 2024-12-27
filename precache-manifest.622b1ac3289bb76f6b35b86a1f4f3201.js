dd17c7bf6b469c6ec2245b4a7033b0b8c99e81c2
Coco <coco@coco.com>
2024-12-27 22:48:54 +0100

    Updates

---
+++ precache-manifest.622b1ac3289bb76f6b35b86a1f4f3201.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dfbd733467663e19658d7cf6dfda4d1",
+    "url": "/dnd-character-sheets-fr-translated/index.html"
+  },
+  {
+    "revision": "296f13718115bdd3235d",
+    "url": "/dnd-character-sheets-fr-translated/static/css/2.d2b82e63.chunk.css"
+  },
+  {
+    "revision": "59666f91e1be5e7ab8e5",
+    "url": "/dnd-character-sheets-fr-translated/static/css/main.152450d1.chunk.css"
+  },
+  {
+    "revision": "296f13718115bdd3235d",
+    "url": "/dnd-character-sheets-fr-translated/static/js/2.b74cd5a2.chunk.js"
+  },
+  {
+    "revision": "30c8d9d509c72bd59e888a2d99a824c7",
+    "url": "/dnd-character-sheets-fr-translated/static/js/2.b74cd5a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59666f91e1be5e7ab8e5",
+    "url": "/dnd-character-sheets-fr-translated/static/js/main.8f6efade.chunk.js"
+  },
+  {
+    "revision": "2ab03062dd0933cfd807",
+    "url": "/dnd-character-sheets-fr-translated/static/js/runtime-main.ad7cd33f.js"
+  },
+  {
+    "revision": "cfa1f07f2b4db70e4f3f8c1cf0e61fa3",
+    "url": "/dnd-character-sheets-fr-translated/static/media/banner.cfa1f07f.png"
+  }
+]);
